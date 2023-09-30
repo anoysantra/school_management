a863debd7e0a261ca696e122cb78f95155f6201c
anoysantra <80537130+anoysantra@users.noreply.github.com>
2023-09-30 22:29:11 +0530

    Update settings.py

--- school_management/settings.py
+++ school_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-76*)q=-lg@7vsc)n%1@hkuz-)jh!vqh8qxdm2c-xgmjslz=fwy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.219.74.101']
+ALLOWED_HOSTS = ['34.219.74.101','.vercel.app']
 
 
 # Application definition
