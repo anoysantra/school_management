fd6643ea6d016b65b78710d45e05889983ef7ebf
anoysantra <80537130+anoysantra@users.noreply.github.com>
2023-10-02 01:32:38 +0530

    Update settings.py

--- school_management/settings.py
+++ school_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-76*)q=-lg@7vsc)n%1@hkuz-)jh!vqh8qxdm2c-xgmjslz=fwy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['34.219.74.101']
 
 
 # Application definition
